8f9be7505babe78f171fe4d7a90b2832ae6e1acd
Jose <chavarriaj84@gmail.com>
2023-08-19 08:51:59 -0400

    v1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+ pool: true,
+  host: "mail.hauscenter.com.bo",
+  port: 465,
+  secure: true, // upgrade later with STARTTLS
+  auth: {
+    user: "noreply@hauscenter.com.bo",
+    pass: "Markas2023*",
+  },
+});
+
+app.post('/api/send-email', async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from: 'chavarriaj84@gmail.com',
+    to: 'chavarriaj84@gmail.com',
+    subject: 'HAUSCENTER',
+    text: 'HAUSCENTER',
+  };
+
+ transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error al enviar el correo:', error);
+  } else {
+    console.log('Correo electrónico enviado:', info.response);
+  }
+});
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});

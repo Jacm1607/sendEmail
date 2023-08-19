d28b5670db018be8dea50aa14f0ac9e12a9b6594
Jose <chavarriaj84@gmail.com>
2023-08-19 09:18:11 -0400

    v1.1

--- index.js
+++ index.js
@@ -6,35 +6,40 @@ const app = express();
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
- pool: true,
-  host: "mail.hauscenter.com.bo",
-  port: 465,
-  secure: true, // upgrade later with STARTTLS
-  auth: {
-    user: "noreply@hauscenter.com.bo",
-    pass: "Markas2023*",
-  },
+    pool: true,
+    host: "mail.hauscenter.com.bo",
+    port: 465,
+    secure: true, // upgrade later with STARTTLS
+    auth: {
+        user: "noreply@hauscenter.com.bo",
+        pass: "Markas2023*",
+    },
 });
+
+
 
 app.post('/api/send-email', async (req, res) => {
-  const { to, subject, text } = req.body;
+    const { to, subject, text } = req.body;
 
-  const mailOptions = {
-    from: 'ecommerce@hauscenter.com.bo',
-    to: 'ecommerce@hauscenter.com.bo',
-    subject: 'NUEVO PEDIDO HAUSCENTER',
-    text: 'HAUSCENTER',
-  };
+    const mailOptions = {
+        from: 'ecommerce@hauscenter.com.bo',
+        to: 'ecommerce@hauscenter.com.bo',
+        subject: 'NUEVO PEDIDO HAUSCENTER',
+        text: 'HAUSCENTER',
+    };
 
- transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.error('Error al enviar el correo:', error);
-  } else {
-    console.log('Correo electrónico enviado:', info.response);
-  }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo:', error);
+        } else {
+            console.log('Correo electrónico enviado:', info.response);
+        }
+    });
+    res.send('proceso completado')
 });
-});
-
+app.get('/', (req, res) => {
+    res.send('email')
+})
 app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+    console.log('Server is running on port 3001');
 });
